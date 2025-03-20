a5272c6b3eabcb1d3f9f3ec04027ece298c8e172
D. de Jong <d.de.jong.3@umail.leidenuniv.nl>
2025-03-20 09:59:58 +0100

    Volgende stap naar het lostrekken van UCB1 en ISMCTS.

--- src/algorithms/bandits/UCB2.py
+++ src/algorithms/bandits/UCB2.py
@@ -2,45 +2,43 @@ from collections.abc import Iterable
 from math import log, sqrt
 from typing import Final
 from algorithms.ISMCTS.determinization import Determinization
-from algorithms.bandits import bandit
+from algorithms.bandits.bandit import Bandit, Node
 
 
-class UCB1(bandit.Bandit):
+class UCB2(Bandit):
     k: Final[float] = 0.75 # (From Cowling et al. (2012))
     
     @classmethod
-    def score(cls, d: Determinization, child: bandit.Node) -> float:
-        print(f"Child.reward(d)={child.reward(d)}, here the d.current_player={d.current_player}")
-
+    def score(cls, d: Determinization, child: Node) -> float:
+        # print(f"Child.reward(d)={child.reward(d)}")
         return child.reward(d)/child.n+cls.k*sqrt(log(child.n_accent)/child.n)
     
     @classmethod
-    def new_score(cls, child: bandit.Node) -> float:
-        print(f"Child.cumul_score={child.cumul_score}")
+    def new_score(cls, child: Node) -> float:
+        # print(f"Child.cumul_score={child.cumul_score}")
         return child.cumul_score/child.n_test+cls.k*sqrt(log(child.n_accent_test)/child.n_test)
     
     @classmethod
-    def choose_arm(cls, node: bandit.Node,
-                   children: Iterable[bandit.Node] = [],
-                   *args) -> bandit.Node:
+    def choose_arm(cls, node: Node,
+                   children: Iterable[Node] = [],
+                   *args) -> Node:
         d: Determinization = args[0]
         old = max(node.children(d), key=lambda v: cls.score(d,v))
         new = max(children, key=lambda v: cls.new_score(v))
-
-        if (old.prev_move != new.prev_move):
-            raise RuntimeError("uhoh")
+        # if (old.prev_move != new.prev_move):
+        #     raise RuntimeError("uhoh")
         
-        return new
+        return old
     
     @classmethod
-    def initialize_node(cls, node: bandit.Node) -> None:
+    def initialize_node(cls, node: Node) -> None:
         node.cumul_score = 0
         node.n_test = 0
         node.n_accent_test = 0
     
     @classmethod
-    def update_node(cls, node: bandit.Node,
-                    children: Iterable[bandit.Node],
+    def update_node(cls, node: Node,
+                    children: Iterable[Node],
                     score: int) -> None:
         node.cumul_score += score
         node.n_test += 1
