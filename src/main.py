60b5e07f60dc13f2896124c38c9d6d7f37b1e65a
D. de Jong <d.de.jong.3@umail.leidenuniv.nl>
2025-03-26 13:15:43 +0100

    Remove all references to Klaverjas and Information Set MCTS, as they have nothing to do with the thesis anymore. (If I decide to use it someday, it's still stashed away in a different branch.)

--- src/main.py
+++ src/main.py
@@ -1,28 +1,9 @@
 import sys
-from games.klaverjas.ISMCTSplayer import ISMCTSPlayer
-from games.klaverjas.humanplayer import HumanPlayer
-from games.klaverjas.klaverjas import KlaverjasGame
 import games.tictactoe as ttt
 
 def main() -> int:
-    player1 = HumanPlayer(0)
-    player2 = ISMCTSPlayer(1)
-    [player3, player4] = [ISMCTSPlayer(i+2) for i in range(0,2)]
-
-    for i in range(4):
-
-        game = KlaverjasGame(player1, player2, player3, player4)
-        winner = game.play()
-
-        print(f"Spel {i}: Spelers ", winner + 1, " en ", winner + 3, " hebben gewonnen met  ", 
-                game.base_points[0], " versus ", game.base_points[1], " punten en ",
-                game.roem[0], " versus ", game.roem[1], " roem!")
-
-    return 0
-
-def tictactoe() -> int:
-    game = ttt.TicTacToe(ttt.CBT1Player(0),ttt.MCTSPlayer(1), print=False)
-    game.set_board_size(3)
+    game = ttt.TicTacToe(ttt.CBT1Player(0),ttt.MCTSPlayer(1), print=True)
+    game.set_board_size(5)
     winner = game.play()
 
     if game.points == 0:
@@ -32,4 +13,4 @@ def tictactoe() -> int:
     return 0
 
 if __name__ == '__main__':
-    sys.exit(tictactoe())
+    sys.exit(main())
